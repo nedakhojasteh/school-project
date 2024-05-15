7d59cdf9b4fd6a23f1e8f6a50d1679b4c5d9f174
Pouya <p.pormohamad@gmail.com>
2024-05-15 22:35:13 +0330

    Feat: implement school store request

---
+++ app/Http/Requests/School/StoreRequest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Requests\School;
+
+use App\Enums\SchoolDistrictEnum;
+use App\Enums\SchoolTypeEnum;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class StoreRequest extends FormRequest
+{
+
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'min:2'],
+            'address' => ['required', 'string', 'min:10', 'max:255'],
+            'telephone' => ['required', 'string', 'size:11'],
+            'class_count' => ['required', 'integer','gte:1'],
+            'type' => ['required', 'string', Rule::enum(SchoolTypeEnum::class)],
+            'district' => ['required', 'string', Rule::enum(SchoolDistrictEnum::class)]
+        ];
+    }
+}
