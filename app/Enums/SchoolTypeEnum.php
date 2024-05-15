799037d1a69add6912322c7d7983bce609c7bfa1
Pouya <p.pormohamad@gmail.com>
2024-05-15 22:30:50 +0330

    Feat: create school type enum

---
+++ app/Enums/SchoolTypeEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum SchoolTypeEnum: string
+{
+    case PRE_SCHOOL = 'PRE_SCHOOL';
+    case ELEMENTARY_SCHOOL = 'ELEMENTARY_SCHOOL';
+    case HIGH_SCHOOL = 'HIGH_SCHOOL';
+
+}
