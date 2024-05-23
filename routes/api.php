825468e9ef332b97e9a15d5f705334886b00eec5
Pouya <p.pormohamad@gmail.com>
2024-05-23 11:24:47 +0330

    feat: implement api store and index Employment

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\EmploymentController;
 use App\Http\Controllers\SchoolController;
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +9,7 @@ Route::post('school/', [SchoolController::class, 'store']);
 Route::get('school/{school}', [SchoolController::class, 'show']);
 Route::put('school/{school}', [SchoolController::class, 'update']);
 Route::delete('school/{school}', [SchoolController::class, 'destroy']);
+
+Route::get('employment/', [EmploymentController::class, 'index']);
+Route::post('employment/', [EmploymentController::class, 'store']);
 
