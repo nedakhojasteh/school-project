1c56578f37bc4520a71ce360bd4ed67629af324d
Pouya <p.pormohamad@gmail.com>
2024-05-23 11:21:31 +0330

    feat: implement EmploymentDegreeEnum

---
+++ app/Enums/EmploymentDegreeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Enums;
+
+enum EmploymentDegreeEnum: string
+{
+    case HIGH_SCHOOL = 'HIGH_SCHOOL';
+    case ASSOCIATE = 'ASSOCIATE';
+    case BACHELOR = 'BACHELOR';
+    case MASTER = 'MASTER';
+    case DOCTORATE = 'DOCTORATE';
+}
